decdf90cf4b73b2447ce166b976c635daa89a688
sudarshansp <111242373+sudarshansp@users.noreply.github.com>
2022-08-20 16:03:55 +0530

    Create prog1.c

---
+++ prog1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int n;
+  printf("Enter a number: ");
+  scanf("%d",&n);
+  if( n > 0 )
+  {
+    printf("Positive Number");
+  }
+  else
+  {
+    printf("non positive");
+  }
+}
